1325640f7b2aeff03d8a08a880aeb6cac63d6d06
Shubham Choudhary <shubhamchoudharyshubh@gmail.com>
2023-05-09 16:16:02 +0530

    Add files via upload

---
+++ loop_pattern_1/num_space.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int n,m;
+     cin>>n>>m;
+     for(int i=1; i<=n; i++){
+                for(int j=1; j<=m; j++){
+                 if(i==1 || j==1 || i==n || j==m){
+                   cout<<j;
+                 }
+                 else
+                 {
+                  cout<<" ";
+                 }
+                }
+                cout<<endl;
+     }
+  return 0;
+}
---
+++ loop_pattern_1/serial_num2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+       int n,m;
+       cin>>n;
+       cin>>m;
+    
+    for(int i=1; i<=n; i++){
+      for(int j=1; j<=m; j++){
+        cout<<j;
+      }
+      cout<<endl;
+    }
+     return 0;
+}
+
