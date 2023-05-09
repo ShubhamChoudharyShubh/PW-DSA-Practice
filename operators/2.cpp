e6ae111923d4b65dfbcc8a6d1fa8514ecff9dcb8
Shubham Choudhary <shubhamchoudharyshubh@gmail.com>
2023-05-09 16:09:57 +0530

    Add files via upload

---
+++ operators/2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+     int x,y;
+     cout<<"inter first num x:";
+     cin>>x;
+     
+     cout<<"inter secound num y:";
+     cin>>y;
+
+    cout<<((x<50)&&(x<y))<<endl;
+
+
+    return 0;
+}
---
+++ operators/6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+   float x,y;
+   cout<<"inter x=";
+   cin>>x;
+   cout<<"inter y=";
+   cin>>y;
+
+   cout<<"sum ="<<x+y<<endl;
+   cout<<"difference ="<<x-y<<endl;
+   cout<<"product ="<<x*y<<endl;
+   cout<<"division ="<<x/y<<endl;
+
+
+
+
+    return 0;
+}
